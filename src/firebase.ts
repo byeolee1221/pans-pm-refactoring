a00710d67a7820ea5532f4b78731a93289bc74d8
byeolee1221 <zicsso1221@gmail.com>
2024-04-15 15:37:24 +0900

    chore: firebaseconfig값 env 적용

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlMMs35LIAKuSjiY0NWxPebl19YazmtLg",
-  authDomain: "pans-pm.firebaseapp.com",
-  projectId: "pans-pm",
-  storageBucket: "pans-pm.appspot.com",
-  messagingSenderId: "1095874793112",
-  appId: "1:1095874793112:web:5f355a7967f50f6eaecaed"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
